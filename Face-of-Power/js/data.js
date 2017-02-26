33dfd4a568563a90b1809fd704b054f2683208db
ZhongxuYang <podo521@qq.com>
2017-02-26 23:55:06 +0800

    2017/2/26

---
+++ Face-of-Power/js/data.js
@@ -0,0 +1,232 @@
+var data = {
+	trump:{
+		svg:{
+			p1:{
+				'd':"M66,153L-92,175L230,457Z",'fill':"rgb(2, 12, 52)",'fill-opacity':"0.74"
+			},p2:{
+				'd':"M33,158L154,59L139,144Z",'fill':"rgb(115, 68, 31)",'fill-opacity':"0.55"
+			},p3:{
+				'd':"M134,161L250,215L170,263Z",'fill':"rgb(3, 13, 45)",'fill-opacity':"0.68"
+			},p4:{
+				'd':"M105,158L50,127L47,41Z",'fill':"rgb(103, 66, 30)",'fill-opacity':"0.61"
+			},p5:{
+				'd':"M52,119L146,118L118,163Z",'fill':"rgb(81, 52, 39)",'fill-opacity':"0.57"
+			},p6:{
+				'd':"M45,53L63,14L124,9Z",'fill':"rgb(139, 107, 40)",'fill-opacity':"0.65"
+			},p7:{
+				'd':"M67,19L115,128L168,27Z",'fill':"rgb(203, 170, 140)",'fill-opacity':"0.95"
+			},p8:{
+				'd':"M107,146L116,271L155,249Z",'fill':"rgb(185, 66, 87)",'fill-opacity':"0.90"
+			},p9:{
+				'd':"M52,146L-86,192L37,-85Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.95"
+			},p10:{
+				'd':"M140,165L172,-32L567,336Z",'fill':"rgb(255, 255, 254)",'fill-opacity':"0.96"
+			},p11:{
+				'd':"M103,159L114,237L50,128Z",'fill':"rgb(245, 244, 245)",'fill-opacity':"0.98"
+			},p12:{
+				'd':"M114,156L97,179L132,180Z",'fill':"rgb(158, 84, 105)",'fill-opacity':"1.00"
+			},p13:{
+				'd':"M107,71L112,80L87,79Z",'fill':"rgb(83, 41, 32)",'fill-opacity':"0.76"
+			},p14:{
+				'd':"M133,112L125,155L151,105Z",'fill':"rgb(64, 48, 34)",'fill-opacity':"0.73"
+			},p15:{
+				'd':"M225,8L181,-157L-18,13Z",'fill':"rgb(255, 255, 253)",'fill-opacity':"0.88"
+			},p16:{
+				'd':"M-2,188L57,140L71,180Z",'fill':"rgb(48, 55, 82)",'fill-opacity':"1.00"
+			},p17:{
+				'd':"M59,104L45,73L47,107Z",'fill':"rgb(189, 165, 146)",'fill-opacity':"1.00"
+			},p18:{
+				'd':"M84,53L45,84L62,16Z",'fill':"rgb(152, 111, 56)",'fill-opacity':"0.50"
+			},p19:{
+				'd':"M60,-183L67,11L-45,179Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.91"
+			},p20:{
+				'd':"M121,7L68,11L172,54Z",'fill':"rgb(172, 159, 101)",'fill-opacity':"0.53"
+			},p21:{
+				'd':"M61,68L162,101L75,120Z",'fill':"rgb(232, 185, 151)",'fill-opacity':"0.72"
+			},p22:{
+				'd':"M132,113L140,60L159,90Z",'fill':"rgb(158, 121, 89)",'fill-opacity':"0.73"
+			},p23:{
+				'd':"M226,133L119,164L157,104Z",'fill':"rgb(253, 254, 254)",'fill-opacity':"0.84"
+			},p24:{
+				'd':"M174,39L212,14L97,-26Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.89"
+			},p25:{
+				'd':"M102,51L156,19L163,48Z",'fill':"rgb(149, 133, 102)",'fill-opacity':"0.58"
+			},p26:{
+				'd':"M121,178L167,266L144,187Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.75"
+			},p27:{
+				'd':"M166,200L181,209L168,207Z",'fill':"rgb(254, 249, 240)",'fill-opacity':"0.52"
+			},p28:{
+				'd':"M132,84L74,58L127,58Z",'fill':"rgb(255, 225, 211)",'fill-opacity':"0.64"
+			},p29:{
+				'd':"M116,145L91,134L98,157Z",'fill':"rgb(187, 151, 132)",'fill-opacity':"0.53"
+			},p30:{
+				'd':"M51,120L85,124L47,101Z",'fill':"rgb(58, 43, 30)",'fill-opacity':"0.60"
+			},p31:{
+				'd':"M105,55L137,61L111,47Z",'fill':"rgb(124, 85, 80)",'fill-opacity':"0.46"
+			},p32:{
+				'd':"M57,157L58,133L49,150Z",'fill':"rgb(43, 50, 67)",'fill-opacity':"0.82"
+			},p33:{
+				'd':"M132,27L98,18L157,20Z",'fill':"rgb(218, 212, 174)",'fill-opacity':"0.74"
+			},p34:{
+				'd':"M33,156L50,147L-34,170Z",'fill':"rgb(244, 245, 244)",'fill-opacity':"0.83"
+			},p35:{
+				'd':"M168,97L145,94L142,110Z",'fill':"rgb(227, 209, 198)",'fill-opacity':"0.54"
+			},p36:{
+				'd':"M135,164L65,162L126,157Z",'fill':"rgb(253, 254, 254)",'fill-opacity':"0.60"
+			},p37:{
+				'd':"M118,196L125,185L113,213Z",'fill':"rgb(236, 224, 250)",'fill-opacity':"0.60"
+			},p38:{
+				'd':"M51,33L78,11L51,52Z",'fill':"rgb(24, 19, 3)",'fill-opacity':"0.42"
+			},p39:{
+				'd':"M105,62L79,30L108,32Z",'fill':"rgb(249, 211, 185)",'fill-opacity':"0.65"
+			},p40:{
+				'd':"M119,114L133,154L92,128Z",'fill':"rgb(97, 67, 52)",'fill-opacity':"0.51"
+			},p41:{
+				'd':"M61,97L59,136L52,124Z",'fill':"rgb(53, 45, 33)",'fill-opacity':"0.43"
+			},p42:{
+				'd':"M119,169L57,141L66,159Z",'fill':"rgb(255, 253, 255)",'fill-opacity':"0.61"
+			},p43:{
+				'd':"M130,121L83,113L133,53Z",'fill':"rgb(245, 198, 185)",'fill-opacity':"0.31"
+			},p44:{
+				'd':"M88,101L128,90L88,96Z",'fill':"rgb(160, 107, 82)",'fill-opacity':"0.54"
+			},p45:{
+				'd':"M117,186L107,167L98,176Z",'fill':"rgb(133, 51, 73)",'fill-opacity':"0.80"
+			},p46:{
+				'd':"M74,121L83,16L52,57Z",'fill':"rgb(255, 194, 109)",'fill-opacity':"0.25"
+			},p47:{
+				'd':"M244,243L187,44L143,121Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.87"
+			},p48:{
+				'd':"M104,180L133,184L124,166Z",'fill':"rgb(72, 1, 23)",'fill-opacity':"0.24"
+			},p49:{
+				'd':"M100,47L85,45L68,62Z",'fill':"rgb(95, 49, 41)",'fill-opacity':"0.41"
+			},p50:{
+				'd':"M57,-36L51,39L37,82Z",'fill':"rgb(252, 252, 252)",'fill-opacity':"0.88"
+			},p51:{
+				'd':"M69,47L68,33L131,39Z",'fill':"rgb(255, 218, 178)",'fill-opacity':"0.40"
+			},p52:{
+				'd':"M45,86L50,64L53,85Z",'fill':"rgb(60, 49, 46)",'fill-opacity':"0.49"
+			},p53:{
+				'd':"M246,44L5,-46L124,11Z",'fill':"rgb(254, 254, 253)",'fill-opacity':"0.89"
+			},p54:{
+				'd':"M107,51L77,89L66,62Z",'fill':"rgb(253, 221, 196)",'fill-opacity':"0.58"
+			},p55:{
+				'd':"M111,185L105,246L97,173Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.53"
+			},p56:{
+				'd':"M124,188L151,298L145,223Z",'fill':"rgb(120, 44, 59)",'fill-opacity':"0.53"
+			},p57:{
+				'd':"M137,173L140,166L133,154Z",'fill':"rgb(30, 33, 48)",'fill-opacity':"0.54"
+			},p58:{
+				'd':"M77,143L63,124L58,129Z",'fill':"rgb(53, 48, 47)",'fill-opacity':"0.49"
+			},p59:{
+				'd':"M71,101L97,121L74,87Z",'fill':"rgb(241, 205, 174)",'fill-opacity':"0.59"
+			},p60:{
+				'd':"M139,177L158,255L121,152Z",'fill':"rgb(246, 248, 250)",'fill-opacity':"0.86"
+			},p61:{
+				'd':"M63,14L46,55L80,22Z",'fill':"rgb(72, 62, 38)",'fill-opacity':"0.33"
+			},p62:{
+				'd':"M57,107L56,87L46,106Z",'fill':"rgb(255, 222, 201)",'fill-opacity':"0.41"
+			},p63:{
+				'd':"M5,164L44,245L48,152Z",'fill':"rgb(46, 52, 78)",'fill-opacity':"0.86"
+			},p64:{
+				'd':"M96,89L88,94L75,95Z",'fill':"rgb(156, 108, 78)",'fill-opacity':"0.57"
+			},p65:{
+				'd':"M62,41L57,80L50,55Z",'fill':"rgb(176, 179, 188)",'fill-opacity':"0.41"
+			},p66:{
+				'd':"M103,77L112,79L108,51Z",'fill':"rgb(72, 2, 0)",'fill-opacity':"0.17"
+			},p67:{
+				'd':"M154,49L262,47L152,77Z",'fill':"rgb(254, 254, 254)",'fill-opacity':"0.79"
+			},p68:{
+				'd':"M59,59L58,136L72,83Z",'fill':"rgb(138, 72, 7)",'fill-opacity':"0.23"
+			},p69:{
+				'd':"M149,37L136,33L152,50Z",'fill':"rgb(198, 197, 163)",'fill-opacity':"0.50"
+			},p70:{
+				'd':"M-2,111L24,33L51,59Z",'fill':"rgb(255, 255, 255)",'fill-opacity':"0.65"
+			},p71:{
+				'd':"M85,110L94,121L74,122Z",'fill':"rgb(153, 112, 79)",'fill-opacity':"0.54"
+			},p72:{
+				'd':"M113,217L123,192L127,204Z",'fill':"rgb(215, 22, 42)",'fill-opacity':"0.57"
+			},p73:{
+				'd':"M162,45L157,23L149,64Z",'fill':"rgb(3, 63, 102)",'fill-opacity':"0.19"
+			},p74:{
+				'd':"M144,79L150,67L149,37Z",'fill':"rgb(186, 186, 170)",'fill-opacity':"0.66"
+			},p75:{
+				'd':"M139,176L125,212L103,256Z",'fill':"rgb(225, 196, 212)",'fill-opacity':"0.86"
+			},p76:{
+				'd':"M95,71L90,81L100,78Z",'fill':"rgb(56, 19, 12)",'fill-opacity':"0.54"
+			},p77:{
+				'd':"M60,121L59,128L57,118Z",'fill':"rgb(173, 142, 127)",'fill-opacity':"0.66"
+			},p78:{
+				'd':"M178,211L167,204L171,201Z",'fill':"rgb(230, 226, 227)",'fill-opacity':"0.48"
+			},p79:{
+				'd':"M123,50L143,48L112,21Z",'fill':"rgb(195, 155, 109)",'fill-opacity':"1.00"
+			},p80:{
+				'd':"M159,25L139,31L151,37Z",'fill':"rgb(126, 124, 94)",'fill-opacity':"0.55"
+			},p81:{
+				'd':"M78,130L138,137L118,118Z",'fill':"rgb(92, 67, 53)",'fill-opacity':"0.65"
+			},p82:{
+				'd':"M151,66L133,125L152,81Z",'fill':"rgb(99, 72, 49)",'fill-opacity':"0.52"
+			},p83:{
+				'd':"M78,64L77,83L90,73Z",'fill':"rgb(255, 238, 220)",'fill-opacity':"0.64"
+			},p84:{
+				'd':"M150,205L140,228L145,202Z",'fill':"rgb(249, 248, 251)",'fill-opacity':"0.59"
+			},p85:{
+				'd':"M105,223L99,253L117,203Z",'fill':"rgb(250, 231, 241)",'fill-opacity':"0.68"
+			},p86:{
+				'd':"M105,7L156,21L83,10Z",'fill':"rgb(169, 160, 115)",'fill-opacity':"0.62"
+			},p87:{
+				'd':"M112,161L112,139L82,146Z",'fill':"rgb(151, 125, 113)",'fill-opacity':"0.67"
+			},p88:{
+				'd':"M125,84L132,61L111,70Z",'fill':"rgb(255, 239, 222)",'fill-opacity':"0.44"
+			},p89:{
+				'd':"M103,169L127,170L118,177Z",'fill':"rgb(174, 25, 42)",'fill-opacity':"0.61"
+			},p90:{
+				'd':"M110,200L110,215L125,176Z",'fill':"rgb(198, 4, 32)",'fill-opacity':"0.46"
+			},p91:{
+				'd':"M85,82L110,79L101,94Z",'fill':"rgb(235, 205, 191)",'fill-opacity':"0.78"
+			},p92:{
+				'd':"M77,28L100,17L130,47Z",'fill':"rgb(232, 185, 137)",'fill-opacity':"0.44"
+			},p93:{
+				'd':"M109,33L105,24L149,32Z",'fill':"rgb(177, 131, 84)",'fill-opacity':"0.55"
+			},p94:{
+				'd':"M142,130L144,106L109,159Z",'fill':"rgb(45, 33, 27)",'fill-opacity':"0.34"
+			},p95:{
+				'd':"M84,55L64,64L65,54Z",'fill':"rgb(68, 27, 0)",'fill-opacity':"0.22"
+			},p96:{
+				'd':"M130,149L141,187L118,161Z",'fill':"rgb(235, 239, 243)",'fill-opacity':"0.74"
+			},p97:{
+				'd':"M90,120L145,106L126,122Z",'fill':"rgb(198, 125, 61)",'fill-opacity':"0.28"
+			},p98:{
+				'd':"M136,212L110,275L136,207Z",'fill':"rgb(224, 226, 253)",'fill-opacity':"0.53"
+			},p99:{
+				'd':"M152,249L184,219L159,196Z",'fill':"rgb(40, 45, 70)",'fill-opacity':"0.50"
+			},p100:{
+				'd':"M156,97L149,110L155,78Z",'fill':"rgb(141, 120, 98)",'fill-opacity':"0.35"
+			}
+		},
+		info:{
+			'Party Affiliation':'Republican Party',
+			'Took Office': 'January 20, 2017 (age 70 years 7 months)',
+            'Challengers': 'Hillary Clinton',
+            'Birthplace': 'New York, New York',
+            'Birthdate': 'June 13, 1946',
+            'State Affiliation': 'New York',
+            'Profession': 'Real estate developer',
+            'Education': 'University of Pennsylvania',
+            'Religion': 'Protestant',
+            'Height': '6\' 3\"'
+		}
+	},
+	obama:{
+		svg:{
+			p1:{
+				
+			},p2:{
+				
+			},p3{
+				
+			},p4{
+				
+			}
+		}
+	}
+}
---
+++ Face-of-Power/js/script.js
@@ -0,0 +1,226 @@
+window.onload = function(){
+	
+/*-------------loading---------------*/
+	(function(){
+		var loading = document.getElementById('loading');
+		var star = loading.getElementsByClassName('blue-star')[0];
+		var prog = loading.getElementsByClassName('prog')[0];
+		
+		//五角星旋转
+		star.style.transform = 'rotate(36000deg)';
+		
+		//红白条的运动
+		setTimeout(function(){
+			prog.style.transition = '3s';
+			prog.style.left = '100vw';
+		},1000);
+		setTimeout(function(){
+			prog.style.transition = 'none';
+			prog.style.left = '-100vw';
+		},4000)
+		setTimeout(function(){
+			prog.style.transition = '3s';
+			prog.style.left = '100vw';
+		},5000);
+		
+		//loading页面消失，welcome显示
+		setTimeout(function(){
+			loading.style.display = 'none';
+			welcome.style.zIndex = '200';
+			welcomeFn();
+		},9000);
+	})();
+	
+/*-------------welcome----------------*/
+	//生成svg图
+	function welcomeFn(){
+		var welcome = document.getElementById('welcome');
+		var paths = welcome.getElementsByTagName('path');
+		for(var i=0;i<sculpture.length;i++){
+			paths[i].setAttribute('d',sculpture[i].d);
+			paths[i].setAttribute('fill',sculpture[i].fill);
+			paths[i].setAttribute('fill-opacity',sculpture[i]['fill-opacity']);
+		}
+		sloganChange();
+	}
+	
+	//slogan的运动函数
+	function sloganChange(){
+		var welcome = document.getElementById('welcome');
+		var slogan = welcome.getElementsByClassName('slogan')[0];
+		var divs = slogan.getElementsByTagName('div');
+		var btn = welcome.getElementsByClassName('btn')[0];
+		var mask = welcome.getElementsByClassName('mask')[0];
+		
+		//红白条运动
+		divs[2].style.left = '100vw';
+		
+		//在红白条运动的时候，更换slogan
+		setTimeout(function(){
+			divs[1].style.display = 'block';
+		},2000);
+		
+		//添加遮罩层以及显示按钮
+		setTimeout(function(){
+			mask.style.opacity = '0.5';
+			btn.style.opacity = 1;
+			btn.style.transform = 'scale(1)';
+		},3000);
+		
+		//按钮点击
+		btn.addEventListener('click',welcomeBtn)
+	}
+	
+	//按钮点击函数
+	function welcomeBtn(){
+		var welcome = document.getElementById('welcome');
+		welcome.style.opacity = '0';
+		setTimeout(function(){
+			welcome.style.display = 'none';
+		},1000);
+	}
+	
+/*-------------personage----------------*/
+	
+	var content = document.getElementById('content');
+	var personage = document.getElementById('personage');
+	personage.btn_onOff = true;
+	
+	//面向对象方式给按钮添加点击事件
+	function BtnClick(obj1,obj2,direction,num){
+		var that = this;
+		this.btn = document.getElementById(obj1);//按钮
+		this.ele = [];
+		obj2.forEach(function(a,b,c){
+			var e = document.getElementById(a);
+			that.ele.push( e );//谁发生改变
+		});
+		this.dir = direction;//改变left还是top
+		this.num = num;//新位置坐标
+		this.onOff = true;//nav的开关
+	}
+	//给按钮的原型绑定事件监听方法
+	BtnClick.prototype.addEvent = function(){
+		var that = this;
+		this.btn.addEventListener('click',function(){
+			that.btnFn(that);
+		});
+	}
+	BtnClick.prototype.btnFn = function(that){
+		if( that.onOff ){
+			that.onOff = false;
+			that.ele.forEach(function(a,b,c){
+				a.style[that.dir[b]] = that.num[b];
+			});
+		}else{
+			that.onOff = true;
+			that.ele.forEach(function(a,b,c){
+				a.style[that.dir[b]] = '';
+			});
+		}
+	}
+	
+	//NAV的点击事件
+	function NavClick(obj1,obj2,direction,num){
+		//属性继承
+		BtnClick.call(this,obj1,obj2,direction,num);
+	};
+	
+	//NAV点击事件的方法继承
+	function Buffer1(){};
+	Buffer1.prototype = BtnClick.prototype;
+	var buffer1 = new Buffer1;
+	NavClick.prototype = buffer1;
+	NavClick.prototype.constructor = NavClick;
+	
+	//NAV独有的新方法
+	NavClick.prototype.down = function(){
+		var that = this;
+		var spans = this.btn.getElementsByTagName('span');
+		this.btn.addEventListener('click',function(){
+			if( that.onOff ){
+				spans[0].style.top = '14px';
+				spans[2].style.top = '14px';
+				spans[1].style.transform = 'rotate(90deg)';
+				that.btn.style.transform = 'rotate(45deg)';
+				content.style.top = '15%';
+			}else{
+				spans[0].style.top = '';
+				spans[2].style.top = '';
+				spans[1].style.transform = '';
+				that.btn.style.transform = '';
+				content.style.top = '';
+			}
+		});
+	};
+	
+	var navClick = new NavClick('nav_btn',['nav'],['top'],['0']);
+	navClick.down();
+	navClick.addEvent();
+	
+	
+	//右侧两个按钮的点击事件
+	function TwoBtnClick(obj1,obj2,direction,num){
+		//属性继承
+		BtnClick.call(this,obj1,obj2,direction,num);
+		this.btns = personage.getElementsByClassName('info')[0].getElementsByClassName('btn');
+		this.btns = Array.prototype.slice.call(this.btns);//类数组转数组
+	};
+	
+	//右侧两个按钮点击事件的方法继承
+	function Buffer2(){};
+	Buffer2.prototype = BtnClick.prototype;
+	var buffer2 = new Buffer2;
+	TwoBtnClick.prototype = buffer2;
+	TwoBtnClick.prototype.constructor = TwoBtnClick;
+	
+	//两个点击按钮独有的新方法
+	TwoBtnClick.prototype.disappear = function(){
+		var that = this;
+		this.btn.addEventListener('click',function(){
+			that.btns.forEach(function(a,b,c){
+				a.style.opacity = '0';
+			});
+		});
+	};
+	TwoBtnClick.prototype.btnFn = function(that){
+		that.ele.forEach(function(a,b,c){
+			a.style[that.dir[b]] = that.num[b];
+		});
+	}
+	var twoBtnClick1 = new TwoBtnClick('info_btn',['resume','official'],['left','bottom'],['0','0']);
+	twoBtnClick1.disappear();
+	twoBtnClick1.addEvent();
+	var twoBtnClick2 = new TwoBtnClick('elections_btn',['resume','official'],['left','bottom'],['0','0']);
+	twoBtnClick2.disappear();
+	twoBtnClick2.addEvent();
+	
+	//关闭按钮的点击事件
+	function CloseClick(obj1,obj2,direction,num){
+		//属性继承
+		BtnClick.call(this,obj1,obj2,direction,num);
+		this.btns = personage.getElementsByClassName('info')[0].getElementsByClassName('btn');
+		this.btns = Array.prototype.slice.call(this.btns);//类数组转数组
+	};
+	
+	//CLOSE点击事件的方法继承
+	function Buffer3(){};
+	Buffer3.prototype = BtnClick.prototype;
+	var buffer3 = new Buffer3;
+	CloseClick.prototype = buffer3;
+	CloseClick.prototype.constructor = CloseClick;
+	
+	//CLOSE独有的新方法
+	CloseClick.prototype.show = function(){
+		var that = this;
+		this.btn.addEventListener('click',function(){
+			that.btns.forEach(function(a,b,c){
+				a.style.opacity = '';
+			});
+		});
+	};
+	
+	var closeClick = new CloseClick('official',['resume','official'],['left','bottom'],['','']);
+	closeClick.show();
+	closeClick.addEvent();
+}
