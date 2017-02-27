eaf6e078da5759bf5c9d515e767ca9ff49dd9848
ZhongxuYang <podo521@qq.com>
2017-02-27 22:59:07 +0800

    new

--- Faces-of-Power/js/script.js
+++ Faces-of-Power/js/script.js
@@ -81,146 +81,190 @@ window.onload = function(){
 	}
 	
 /*-------------personage----------------*/
-	
 	var content = document.getElementById('content');
 	var personage = document.getElementById('personage');
-	personage.btn_onOff = true;
-	
-	//面向对象方式给按钮添加点击事件
-	function BtnClick(obj1,obj2,direction,num){
-		var that = this;
-		this.btn = document.getElementById(obj1);//按钮
-		this.ele = [];
-		obj2.forEach(function(a,b,c){
-			var e = document.getElementById(a);
-			that.ele.push( e );//谁发生改变
-		});
-		this.dir = direction;//改变left还是top
-		this.num = num;//新位置坐标
-		this.onOff = true;//nav的开关
-	}
-	//给按钮的原型绑定事件监听方法
-	BtnClick.prototype.addEvent = function(){
-		var that = this;
-		this.btn.addEventListener('click',function(){
-			that.btnFn(that);
-		});
-	}
-	BtnClick.prototype.btnFn = function(that){
-		if( that.onOff ){
-			that.onOff = false;
+	(function(){
+		personage.btn_onOff = true;
+		
+		//面向对象方式给按钮添加点击事件
+		function BtnClick(obj1,obj2,direction,num){
+			var that = this;
+			this.btn = document.getElementById(obj1);//按钮
+			this.ele = [];
+			obj2.forEach(function(a,b,c){
+				var e = document.getElementById(a);
+				that.ele.push( e );//谁发生改变
+			});
+			this.dir = direction;//改变left还是top
+			this.num = num;//新位置坐标
+			this.onOff = true;//nav的开关
+		}
+		//给按钮的原型绑定事件监听方法
+		BtnClick.prototype.addEvent = function(){
+			var that = this;
+			this.btn.addEventListener('click',function(){
+				that.btnFn(that);
+			});
+		}
+		BtnClick.prototype.btnFn = function(that){
+			if( that.onOff ){
+				that.onOff = false;
+				that.ele.forEach(function(a,b,c){
+					a.style[that.dir[b]] = that.num[b];
+				});
+			}else{
+				that.onOff = true;
+				that.ele.forEach(function(a,b,c){
+					a.style[that.dir[b]] = '';
+				});
+			}
+		}
+		
+		//NAV的点击事件
+		function NavClick(obj1,obj2,direction,num){
+			//属性继承
+			BtnClick.call(this,obj1,obj2,direction,num);
+		};
+		
+		//NAV点击事件的方法继承
+		function Buffer1(){};
+		Buffer1.prototype = BtnClick.prototype;
+		var buffer1 = new Buffer1;
+		NavClick.prototype = buffer1;
+		NavClick.prototype.constructor = NavClick;
+		
+		//NAV独有的新方法
+		NavClick.prototype.down = function(){
+			var that = this;
+			var spans = this.btn.getElementsByTagName('span');
+			this.btn.addEventListener('click',function(){
+				if( that.onOff ){
+					spans[0].style.top = '14px';
+					spans[2].style.top = '14px';
+					spans[1].style.transform = 'rotate(90deg)';
+					that.btn.style.transform = 'rotate(45deg)';
+					content.style.top = '15%';
+				}else{
+					spans[0].style.top = '';
+					spans[2].style.top = '';
+					spans[1].style.transform = '';
+					that.btn.style.transform = '';
+					content.style.top = '';
+				}
+			});
+		};
+		
+		var navClick = new NavClick('nav_btn',['nav'],['top'],['0']);
+		navClick.down();
+		navClick.addEvent();
+		
+		
+		//右侧两个按钮的点击事件
+		function TwoBtnClick(obj1,obj2,direction,num){
+			//属性继承
+			BtnClick.call(this,obj1,obj2,direction,num);
+			this.btns = personage.getElementsByClassName('info')[0].getElementsByClassName('btn');
+			this.btns = Array.prototype.slice.call(this.btns);//类数组转数组
+		};
+		
+		//右侧两个按钮点击事件的方法继承
+		function Buffer2(){};
+		Buffer2.prototype = BtnClick.prototype;
+		var buffer2 = new Buffer2;
+		TwoBtnClick.prototype = buffer2;
+		TwoBtnClick.prototype.constructor = TwoBtnClick;
+		
+		//两个点击按钮独有的新方法
+		TwoBtnClick.prototype.disappear = function(){
+			var that = this;
+			this.btn.addEventListener('click',function(){
+				that.btns.forEach(function(a,b,c){
+					a.style.opacity = '0';
+				});
+			});
+		};
+		TwoBtnClick.prototype.btnFn = function(that){
 			that.ele.forEach(function(a,b,c){
 				a.style[that.dir[b]] = that.num[b];
 			});
-		}else{
-			that.onOff = true;
-			that.ele.forEach(function(a,b,c){
-				a.style[that.dir[b]] = '';
-			});
 		}
-	}
-	
-	//NAV的点击事件
-	function NavClick(obj1,obj2,direction,num){
-		//属性继承
-		BtnClick.call(this,obj1,obj2,direction,num);
-	};
-	
-	//NAV点击事件的方法继承
-	function Buffer1(){};
-	Buffer1.prototype = BtnClick.prototype;
-	var buffer1 = new Buffer1;
-	NavClick.prototype = buffer1;
-	NavClick.prototype.constructor = NavClick;
-	
-	//NAV独有的新方法
-	NavClick.prototype.down = function(){
-		var that = this;
-		var spans = this.btn.getElementsByTagName('span');
-		this.btn.addEventListener('click',function(){
-			if( that.onOff ){
-				spans[0].style.top = '14px';
-				spans[2].style.top = '14px';
-				spans[1].style.transform = 'rotate(90deg)';
-				that.btn.style.transform = 'rotate(45deg)';
-				content.style.top = '15%';
-			}else{
-				spans[0].style.top = '';
-				spans[2].style.top = '';
-				spans[1].style.transform = '';
-				that.btn.style.transform = '';
-				content.style.top = '';
-			}
-		});
-	};
-	
-	var navClick = new NavClick('nav_btn',['nav'],['top'],['0']);
-	navClick.down();
-	navClick.addEvent();
-	
-	
-	//右侧两个按钮的点击事件
-	function TwoBtnClick(obj1,obj2,direction,num){
-		//属性继承
-		BtnClick.call(this,obj1,obj2,direction,num);
-		this.btns = personage.getElementsByClassName('info')[0].getElementsByClassName('btn');
-		this.btns = Array.prototype.slice.call(this.btns);//类数组转数组
-	};
-	
-	//右侧两个按钮点击事件的方法继承
-	function Buffer2(){};
-	Buffer2.prototype = BtnClick.prototype;
-	var buffer2 = new Buffer2;
-	TwoBtnClick.prototype = buffer2;
-	TwoBtnClick.prototype.constructor = TwoBtnClick;
-	
-	//两个点击按钮独有的新方法
-	TwoBtnClick.prototype.disappear = function(){
-		var that = this;
-		this.btn.addEventListener('click',function(){
-			that.btns.forEach(function(a,b,c){
-				a.style.opacity = '0';
+		var twoBtnClick1 = new TwoBtnClick('info_btn',['resume','official'],['left','bottom'],['0','0']);
+		twoBtnClick1.disappear();
+		twoBtnClick1.addEvent();
+		var twoBtnClick2 = new TwoBtnClick('elections_btn',['resume','official'],['left','bottom'],['0','0']);
+		twoBtnClick2.disappear();
+		twoBtnClick2.addEvent();
+		
+		//关闭按钮的点击事件
+		function CloseClick(obj1,obj2,direction,num){
+			//属性继承
+			BtnClick.call(this,obj1,obj2,direction,num);
+			this.btns = personage.getElementsByClassName('info')[0].getElementsByClassName('btn');
+			this.btns = Array.prototype.slice.call(this.btns);//类数组转数组
+		};
+		
+		//CLOSE点击事件的方法继承
+		function Buffer3(){};
+		Buffer3.prototype = BtnClick.prototype;
+		var buffer3 = new Buffer3;
+		CloseClick.prototype = buffer3;
+		CloseClick.prototype.constructor = CloseClick;
+		
+		//CLOSE独有的新方法
+		CloseClick.prototype.show = function(){
+			var that = this;
+			this.btn.addEventListener('click',function(){
+				that.btns.forEach(function(a,b,c){
+					a.style.opacity = '';
+				});
 			});
+		};
+		
+		var closeClick = new CloseClick('official',['resume','official'],['left','bottom'],['','']);
+		closeClick.show();
+		closeClick.addEvent();
+	})();
+	
+/*-------------personage-canvas---------------*/
+	
+	(function(){
+		var canvas = personage.getElementsByClassName('canvas')[0];
+		var svgs = canvas.getElementsByTagName('svg');
+		var clipPaths = canvas.getElementsByTagName('clipPath');
+		var bg = canvas.getElementsByClassName('bg')[0];
+		var paths = canvas.getElementsByTagName('path');
+		var imgs = personage.getElementsByClassName('bar-imgs')[0].getElementsByTagName('div');
+		imgs = Array.from(imgs);//类数组转数组
+		svgs = Array.from(svgs);
+		paths = Array.from(paths);
+		clipPaths = Array.from(clipPaths);
+		
+		imgs.forEach(function(a,b,c){
+			a.addEventListener('click',clickFn);
 		});
-	};
-	TwoBtnClick.prototype.btnFn = function(that){
-		that.ele.forEach(function(a,b,c){
-			a.style[that.dir[b]] = that.num[b];
-		});
-	}
-	var twoBtnClick1 = new TwoBtnClick('info_btn',['resume','official'],['left','bottom'],['0','0']);
-	twoBtnClick1.disappear();
-	twoBtnClick1.addEvent();
-	var twoBtnClick2 = new TwoBtnClick('elections_btn',['resume','official'],['left','bottom'],['0','0']);
-	twoBtnClick2.disappear();
-	twoBtnClick2.addEvent();
-	
-	//关闭按钮的点击事件
-	function CloseClick(obj1,obj2,direction,num){
-		//属性继承
-		BtnClick.call(this,obj1,obj2,direction,num);
-		this.btns = personage.getElementsByClassName('info')[0].getElementsByClassName('btn');
-		this.btns = Array.prototype.slice.call(this.btns);//类数组转数组
-	};
-	
-	//CLOSE点击事件的方法继承
-	function Buffer3(){};
-	Buffer3.prototype = BtnClick.prototype;
-	var buffer3 = new Buffer3;
-	CloseClick.prototype = buffer3;
-	CloseClick.prototype.constructor = CloseClick;
-	
-	//CLOSE独有的新方法
-	CloseClick.prototype.show = function(){
-		var that = this;
-		this.btn.addEventListener('click',function(){
-			that.btns.forEach(function(a,b,c){
-				a.style.opacity = '';
+		function clickFn(ev){
+			var name = ev.target.className;
+			svgs.forEach(function(a,b,c){
+				a.setAttribute('viewBox',data[name].other.viewBox);
 			});
-		});
-	};
-	
-	var closeClick = new CloseClick('official',['resume','official'],['left','bottom'],['','']);
-	closeClick.show();
-	closeClick.addEvent();
+			clipPaths.forEach(function(a,b,c){
+				a.children[0].setAttribute('width',data[name].other.width);
+				a.children[0].setAttribute('height',data[name].other.height);
+			});
+			canvas.style.backgroundColor = data[name].other.fill;
+			bg.setAttribute('width',data[name].other.width);
+			bg.setAttribute('height',data[name].other.height);
+			bg.setAttribute('fill',data[name].other.fill);
+			paths.forEach(function(a,b,c){
+				a.setAttribute('d',data[name].svg['p'+(b+1)].d);
+				a.setAttribute('fill',data[name].svg['p'+(b+1)].fill);
+				a.setAttribute('fill-opacity',data[name].svg['p'+(b+1)]['fill-opacity']);
+			});
+		};
+		
+	})();
+
+
 }
+
+
