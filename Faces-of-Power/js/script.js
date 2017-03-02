71905e21a31aa7ada9611dd6366a6a7856b9fbfb
ZhongxuYang <podo521@qq.com>
2017-03-02 23:41:38 +0800

    new

--- Faces-of-Power/js/script.js
+++ Faces-of-Power/js/script.js
@@ -8,7 +8,23 @@ window.onload = function(){
 		a.style.left = 80*b + 'px';
 	})
 	bar_img.style.left = -(3600 - winW)/2 + 'px';
-//	grayscale(bar_imgs.getElementsByTagName("div"));
+//	grayscale(bar_imgs.getElementsByTagName("div"));/*-----灰色蒙版------*/
+
+/*-------------nav跳转---------------*/
+	(function(){
+		var nav = document.getElementById('nav');
+		var btns = nav.getElementsByTagName('a');
+		btns[0].addEventListener('click',function(){
+			location.href = 'https://github.com/ZhongxuYang/';
+		});
+		btns[1].addEventListener('click',function(){
+			location.href = 'https://zhongxuyang.github.io/Faces-of-Power/';
+		});
+		btns[2].addEventListener('click',function(){
+			location.href = 'https://zhongxuyang.github.io/Faces-of-Power/';
+		});
+	})();
+
 /*-------------loading---------------*/
 	(function(){
 		var loading = document.getElementById('loading');
